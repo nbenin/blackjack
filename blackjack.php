434c030a479c32aca524199f1337c105995b275a
Neil <neil.mf.benin@gmail.com>
2020-02-13 10:15:28 +0100

    preparing files and pseudo code

---
+++ blackjack.php
@@ -0,0 +1,10 @@
+<?php
+// Yes
+declare(strict_types=1);
+// Base blackjack class
+class Blackjack {
+
+}
+
+require 'game.php';
+require 'index.php';
---
+++ game.php
@@ -0,0 +1,3 @@
+<?php
+require 'blackjack.php';
+require 'index.php';
